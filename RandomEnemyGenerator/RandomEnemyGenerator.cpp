c1b98db3c70487d3e06a0259dd36b0b74dc24c07
adriaortizmartinez <adriaortizmartinez@S3-03>
2017-02-21 10:45:34 +0100

    Ejercicio bien C++eado

--- RandomEnemyGenerator/RandomEnemyGenerator.cpp
+++ RandomEnemyGenerator/RandomEnemyGenerator.cpp
@@ -6,56 +6,69 @@
 #include <string>
 #include <ctime>
 
-enum class EnemyType {zombie, vampire, ghost, witch};
+enum class EnemyType {ZOMBIE, VAMPIRE, GHOST, WITCH, MAX};
+
 struct Enemy {
 	EnemyType type;
 	std::string name;
 	int health;
+
+	std::string getEnemyTypeString() {
+		switch (type) {
+		case EnemyType::ZOMBIE:
+			return "zombie";
+		case EnemyType::VAMPIRE:
+			return "vampire";
+		case EnemyType::GHOST:
+			return "ghost";
+		case EnemyType::WITCH:
+			return "witch";
+		default:
+			return "";
+		}
+	}
 };
 
-Enemy CreateRandomEnemy() {
-	srand(time(NULL)); //Altera la seed
-	Enemy evil;
-	evil.type = EnemyType(rand() % 4);
-	evil.health = (rand() % 501) + 100;
-	switch (rand() % 8)
-	{
-	case 0:
-		evil.name = "Hipolito";
-		break;
-	case 1:
-		evil.name = "Fulgencia";
-		break;
-	case 2:
-		evil.name = "Pancracia";
-		break;
-	case 3:
-		evil.name = "Eustaquio";
-		break;
-	case 4:
-		evil.name = "Pocahonto";
-		break;
-	case 5:
-		evil.name = "Ambrosio";
-		break;
-	case 6:
-		evil.name = "Radev";
-		break;
-	case 7:
-		evil.name = "Willy";
-		break;
-	default:
-		break;
-	}
+bool operator==(const Enemy &a, const Enemy &b) { //Operator == overloaded
+	return a.name == b.name && a.type == b.type;
 }
 
-bool Operador(Enemy evil1, Enemy evil2) {
-	if (evil1)
+static Enemy CreateRandomEnemy() {
+	srand(time(NULL)); //Altera la seed
+	static const int MAX_LIFE{ 500 }; //Vida maxima de un enemigo
+	static const std::string NAMES[]{ "Gertrudis", "Pancracia", "Anacleto", "Hipolito", "Eustaquio", "Fulgencia", "Radev", "Willy", "Pocahonto", "Ambrosio", "Margarita", "Lesinga", "Tito Shen", "Poppy Pintame" }; //Lista de posibles nombres de enemigos
+	return Enemy{ //Devuelve un enemigo con atributos aleatorios. {} nos permite construir la variable en su declaración, y sus componentes deben estar ordenados según la declaración del struct
+		static_cast<EnemyType>(rand() % static_cast<int>(EnemyType::MAX)),
+		NAMES[rand() % (sizeof NAMES / sizeof std::string)],
+		rand() % MAX_LIFE
+	};
 }
 
 int main()
 {
+	srand(static_cast<unsigned>(time(nullptr)));
+	const int MAX_ENEMIES{ 5 };
+	Enemy enemies[MAX_ENEMIES];
+	int i{ 0 };
+	while (i < MAX_ENEMIES) {
+		enemies[i] = CreateRandomEnemy();
+		int j{ i - 1 };
+		while (j >= 0) {
+			if (enemies[i] == enemies[j])
+			{
+				--i;
+				break;
+			}
+			j--;
+		}
+		++i;
+	}
 
+	//Print the array of enemies
+	std::cout << "List of enemies:\n";
+	for (auto &enemy : enemies) {
+		std::cout << enemy.name << " is a " << enemy.getEnemyTypeString() << " whose life is " << enemy.health << std::endl;
+	}
     return 0;
 }
 
